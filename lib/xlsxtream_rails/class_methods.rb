b938ec33b87bb1eadae470cbfa5dddae952541af
doabit <doinsist@gmail.com>
2022-07-11 10:29:20 +0800

    feat: add except_xlsx_columns

--- lib/xlsxtream_rails/class_methods.rb
+++ lib/xlsxtream_rails/class_methods.rb
@@ -2,7 +2,11 @@ module XlsxtreamRails
   module ClassMethods
     # TODO
     def to_xlsx
-      column_names.map { |x| x.to_sym }
+      (column_names - except_xlsx_columns).map { |x| x.to_sym }
+    end
+
+    def except_xlsx_columns
+      []
     end
   end
 end
